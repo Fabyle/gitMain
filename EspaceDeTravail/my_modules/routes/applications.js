d83e58fd2f9ad36fb1ba36c52d4be948fe2ed451
fabien <fabien99@gmail.com>
2016-06-21 11:04:36 +0200

    mise au point

--- EspaceDeTravail/my_modules/routes/applications.js
+++ EspaceDeTravail/my_modules/routes/applications.js
@@ -3,16 +3,18 @@ var ArrayList = require('ArrayList');
 
 var applications = new ArrayList;
 
-applications.add({ "nom" : "visioCAP",
-					"configurations" : {
+applications.add({ 	"id"				:	"00001",
+					"nom" 				: "visiocap",
+					"configurations" 	: {
 						"urlCTG"	: 	"http://toto",
 						"log"		:	"c:log.txt"
 					}});
 					
-applications.add({ "nom" : "ESIC",
-						"configurations" : {
-							"urlCTG"	: 	"http://toto",
-							"log"		:	"c:log.txt"
+applications.add({ 	"id"				:	"00002",
+					"nom" 				: "esic",
+					"configurations" 	: {
+						"urlCTG"	: 	"http://toto",
+						"log"		:	"c:log.txt"
 						}});
 
 //==================================================
@@ -20,24 +22,34 @@ applications.add({ "nom" : "ESIC",
 //==================================================
 var getApplications = 
 	function (req, res){
-		if (!req.query.nom_application) 
+		if (!req.query.nom)
 			{// le nom de l'application n'est pas renseigné
 			// on retourne toutes les applications
 			res.send(applications);	
-			console.log(applications);
 			}
 		else {
 			// le nom de l'application est renseigné
 			// on retourne les applications vérifiant ce nom.
 			sousListe = applications.find (function (application){
-				return application.nom === req.query.nom_application;
+				return application.nom === req.query.nom;
 			})
 			res.send(sousListe);
 		}
 		
-	};	
+	};
+	
+var getApplication = 
+	function (req, res){
+	if (!req.query.nom_application) 
+		{// le nom de l'application n'est pas renseigné
+		// on retourne toutes les applications
+		res.send(applications);	
+		}
+};
+	
 
 //==================================================
 //Les exports
 //==================================================
 exports.getApplications = getApplications
+exports.getApplication = getApplication
