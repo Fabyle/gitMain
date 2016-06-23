520401555b30dafe58364b908c8a9658d6d03744
fabien <fabien99@gmail.com>
2016-06-23 11:31:49 +0200

    ajout d'un timestamp sur l'état

--- EspaceDeTravail/my_modules/business/application_etat.js
+++ EspaceDeTravail/my_modules/business/application_etat.js
@@ -6,10 +6,12 @@ var etat_possible = [ "inconnu", "a_demarrer", "demarree", "a arreter", "arretee
 
 function application_etat_inconnu(){
 	this.status = "inconnu";
+	this.time_stamp = (new Date()).toLocaleString();
 }
 
 function set_etat_a_demarrer(){
 	this.status = "a_demarrer";
+	this.time_stamp = (new Date()).toLocaleString();
 }
 
 exports.inconnu = application_etat_inconnu;
